9cb9fe5e979753cd53cf6bcc88705bec3ed99525
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-09-15 11:33:32 +0530

    FSA

---
+++ PYTHON/class-12[function-2]/crtway.py
@@ -0,0 +1,40 @@
+# first way
+def user_module():
+    def login():
+        print("login success")
+    def logout():
+        print("logout success")
+    return True
+x=user_module()
+print(x)
+
+
+# SEcond way
+def user_module():
+    def login():
+        print("login success")
+    def logout():
+        print("logout success")
+    return login() #---here we are using login() to be printed which returns None
+x=user_module()
+print(x) #----None
+
+# third way
+def user_module():
+    def login():
+        print("login success")
+    def logout():
+        print("logout success")
+    return login()
+x=user_module
+print(x)
+
+# fourth way
+def user_module():
+    def login():
+        print("login success")
+    def logout():
+        print("logout success")
+    return login
+x=user_module()
+x()
