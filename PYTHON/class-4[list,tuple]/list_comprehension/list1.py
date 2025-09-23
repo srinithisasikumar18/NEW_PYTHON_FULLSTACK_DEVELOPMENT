43fb543932ce550f92961f1e3a743083f5304697
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-09-23 12:26:06 +0530

    FSA

---
+++ PYTHON/class-4[list,tuple]/list_comprehension/list1.py
@@ -0,0 +1,6 @@
+even_num=[]
+numbers=[1,2,3,4,5,6,7,8,9,10]
+for num in numbers:
+    if num%2==0:
+        even_num.append(num)
+print(even_num)
---
+++ PYTHON/class-4[list,tuple]/list_comprehension/one.py
@@ -0,0 +1,3 @@
+numbers=[1,2,3,4,5,6,7,10,89,6]
+even_num=[nums for nums in numbers if nums%2==0]
+print(even_num)
