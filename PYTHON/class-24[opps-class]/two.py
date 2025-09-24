4a5a247e45f1ed7304f66b91cc48fbbbaae9ef97
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-09-24 17:15:36 +0530

    PYTHON

---
+++ PYTHON/class-24[opps-class]/two.py
@@ -0,0 +1,10 @@
+class Account:
+    def open_account(self):
+        print("Account opened Successfully!")
+    def deposit_amount(self):
+        print("Amount Deposited")
+    def get_Bal(self):
+        return 0
+a1=Account()
+a1.open_account()
+a1.deposit_amount()
