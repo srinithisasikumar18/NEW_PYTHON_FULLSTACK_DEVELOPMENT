4eca63635831fc7e571eb534fb0578a10cd89ba8
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-09-24 17:13:46 +0530

    PYTHON

---
+++ PYTHON/class-23[opps-class]/bank.py
@@ -0,0 +1,18 @@
+class Account:
+    pass
+    def open_ac(self):
+        print("Account opened")
+    def depoist(self):
+        print("Amount deposited")
+    def withdraw(self):
+        print("Amount Withdrawed")
+    def get_bal(self):
+        print("Available Balance")
+    def close_acc(self):
+        print("Account closed")
+a1=Account()
+a1.open_ac()
+a1.depoist()
+a1.withdraw()
+a1.get_bal()
+a1.close_acc()
--- PYTHON/class-23[opps-class]/one.py
+++ PYTHON/class-23[opps-class]/one.py
@@ -2,5 +2,16 @@ class Account:
     pass
 a1=Account()
 a2=Account()
+a3=Account()
+a4=Account()
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
 print(id(a1))
 print(id(a2))
+print(id(a3))
+print(id(a4))
+
+print(type(a1))
