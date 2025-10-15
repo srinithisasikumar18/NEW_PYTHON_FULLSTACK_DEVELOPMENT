c10e47b88fde28f162e101d9ab56774af7700171
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-10-15 10:21:51 +0530

    jS

---
+++ JAVASCRIPT/class-3[Non-primitive DT]/crud.js
@@ -0,0 +1,10 @@
+let ename=["rahul","srinithi","kokila","sasi","karthik","kamal"]
+//read
+console.log(ename)
+// update
+ename[0]="uma"
+
+console.log(ename)
+// delete
+delete ename[9]
+console.log(ename)
---
+++ JAVASCRIPT/class-3[Non-primitive DT]/crud_obj.js
@@ -0,0 +1,24 @@
+var a={}
+var emp={
+    "eid":101,
+    "ename":"srinithi",
+    "easl":45000.50
+}
+
+// READ
+
+console.log(emp)
+console.log(a)
+console.log(typeof a)
+console.log(typeof emp)
+console.log(emp.eid)
+console.log(emp.ename)
+
+// UPDATE
+
+emp.esal=50000.50;
+console.log(emp)
+emp.gender="Female";
+console.log(emp)
+
+// 
