bcf474db52cd80a566d5d901262af7afc0d6cdf1
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-10-15 09:54:57 +0530

    jS

---
+++ JAVASCRIPT/class-3[Non-primitive DT]/one_obj.js
@@ -0,0 +1,11 @@
+let ename=["rahul","srinithi","kokila","sasi"]
+console.log(ename[0])
+console.log(ename[1])
+console.log(ename[2])
+console.log(ename[3])
+// undefined
+// undefined
+// undefined
+console.log(ename[4])
+console.log(ename[-1])
+console.log(ename[5])
