8718397606ee43f2c0893713bf5f07950e47a9d5
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-10-15 10:01:37 +0530

    jS

---
+++ JAVASCRIPT/class-3[Non-primitive DT]/delete_onj.js
@@ -0,0 +1,3 @@
+let ename=["rahul","srinithi","kokila","sasi"]
+ename.pop()
+console.log(ename)
---
+++ JAVASCRIPT/class-3[Non-primitive DT]/update.js
@@ -0,0 +1,3 @@
+let ename=["rahul","srinithi","kokila","sasi"]
+ename[0]="karthik"
+console.log(ename)
