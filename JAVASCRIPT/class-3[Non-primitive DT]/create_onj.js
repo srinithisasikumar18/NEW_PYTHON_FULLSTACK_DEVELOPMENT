278b10a7ceea777da723c2d7117f7e961c1ad346
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-10-15 09:50:33 +0530

    jS

---
+++ JAVASCRIPT/class-3[Non-primitive DT]/create_onj.js
@@ -0,0 +1,9 @@
+var eid=[10,20,30,40,50]
+var a=[10,10,20,20,30,30]
+var b=["srinithi",10,20.5,true,undefined,[10,20,300],{},"S"]
+console.log(eid)
+console.log(a)
+console.log(b)
+console.log(typeof eid)
+console.log(typeof a)
+console.log(typeof b)
