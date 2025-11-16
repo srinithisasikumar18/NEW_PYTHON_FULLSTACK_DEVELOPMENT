3df246ad9ca046563a57b788f3007953818c3ba1
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-11-16 19:33:23 +0530

    FSP

---
+++ JAVASCRIPT_TASKS/TASK-ES6 Array Methods/b.js
@@ -0,0 +1,4 @@
+// 2. Use map() to create a new array containing the lengths of each name. 
+let enames = ["rahul", "sonia", "priya", "modi"]
+let name_length = enames.map(name => name.length)
+console.log(name_length)
---
+++ JAVASCRIPT_TASKS/TASK-ES6 Array Methods/c.js
@@ -0,0 +1,1 @@
+let enames = ["rahul", "sonia", "priya", "modi"] 
