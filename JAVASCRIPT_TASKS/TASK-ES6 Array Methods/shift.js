304509165a11d78b2d764119594d9cfff3cc903f
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2025-11-16 22:03:18 +0530

    FSP

---
+++ JAVASCRIPT_TASKS/TASK-ES6 Array Methods/shift.js
@@ -0,0 +1,3 @@
+let enames = ["rahul", "sonia", "priya", "modi"]
+enames.shift()
+console.log(enames)
---
+++ JAVASCRIPT_TASKS/TASK-ES6 Array Methods/sort.js
@@ -0,0 +1,3 @@
+let enames = ["rahul", "sonia", "priya", "modi"]
+enames.sort()
+console.log(enames)
